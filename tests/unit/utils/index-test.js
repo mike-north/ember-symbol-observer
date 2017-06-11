ee60c39c09a25ecb668193214bb482a617efc649
toran billups <toranb@gmail.com>
2017-06-11 07:25:13 -0500

    [BUILD]: removed deploy from travis.yml

--- tests/unit/utils/index-test.js
+++ tests/unit/utils/index-test.js
@@ -4,8 +4,7 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | index');
 
-// Replace this with your real tests.
 test('it works', function(assert) {
-  assert.ok(so, 'symbol-observable module is present');
-  assert.ok(sop, 'symbol-observable/ponyfill module is present');
+  assert.equal(typeof so, 'string');
+  assert.equal(typeof sop, 'function');
 });
